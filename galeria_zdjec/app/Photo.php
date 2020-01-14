fc12c5a88c8b4cd0f55c9cfcc4b02fa4ccee88a8
Student <pawelwas1997@gmail.com>
2020-01-14 23:28:31 +0100

    Removed duplicates

--- galeria_zdjec/app/Photo.php
+++ galeria_zdjec/app/Photo.php
@@ -22,8 +22,6 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Photo whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Photo whereUserID($value)
  * @mixin \Eloquent
- * @property int $user_id
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Photo whereUserId($value)
  */
 class Photo extends Model
 {
