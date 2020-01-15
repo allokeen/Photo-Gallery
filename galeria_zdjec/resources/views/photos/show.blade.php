bff602820cc5eca461f64f1ee29afdecb84b1f6b
Student <nikswieagh@gmail.com>
2020-01-15 15:28:36 +0100

    Fixing Delete feature

--- galeria_zdjec/resources/views/photos/show.blade.php
+++ galeria_zdjec/resources/views/photos/show.blade.php
@@ -6,6 +6,10 @@
         <p>{{$photo->description}}</p>
         <a href={{ route('photos.edit', $photo) }}>Edytuj</a>
         <br>
-        <a href={{ route('photos.destroy', $photo) }}>Usuń</a>
+        <form method="post" action="{{ route('photos.destroy', $photo) }}">
+            {{ method_field('DELETE') }}
+            {{ csrf_field() }}
+            <input type="submit" value="Delete">
+        </form>
     </div>
 @endsection
